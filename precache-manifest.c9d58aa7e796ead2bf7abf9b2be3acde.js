9d84b41ac3bf7d206879c9a4eebe276875364dfa
Ameer Ashif K <ameer.asif777@gmail.com>
2021-06-06 14:16:59 +0530

    Updates

---
+++ precache-manifest.c9d58aa7e796ead2bf7abf9b2be3acde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12dee8cec4296690b4e9c5dcd2883f0",
+    "url": "/pwa-react-app/index.html"
+  },
+  {
+    "revision": "b6f40c01927f27c25367",
+    "url": "/pwa-react-app/static/css/main.c3e07cbb.chunk.css"
+  },
+  {
+    "revision": "1a7874feb5c97710454d",
+    "url": "/pwa-react-app/static/js/2.7c921383.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pwa-react-app/static/js/2.7c921383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f40c01927f27c25367",
+    "url": "/pwa-react-app/static/js/main.2e5c5073.chunk.js"
+  },
+  {
+    "revision": "05375a06d118cfed7c0e",
+    "url": "/pwa-react-app/static/js/runtime-main.90bab717.js"
+  }
+]);
